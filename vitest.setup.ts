709a06a83a9050ce29639e1e2d62cc2907129ca0
bodangren <bodangren@gmail.com>
2025-11-08 10:47:07 +0800

    feat: migrate chart visualizations (#33) (#51)

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -5,3 +5,35 @@ import { cleanup } from '@testing-library/react';
 afterEach(() => {
   cleanup();
 });
+
+class ResizeObserver {
+  callback: ResizeObserverCallback;
+
+  constructor(callback: ResizeObserverCallback) {
+    this.callback = callback;
+  }
+
+  observe(target: Element) {
+    this.callback(
+      [
+        {
+          contentRect: {
+            width: (target as HTMLElement).clientWidth || 600,
+            height: (target as HTMLElement).clientHeight || 400
+          }
+        } as ResizeObserverEntry
+      ],
+      this
+    );
+  }
+
+  unobserve() {
+    // no-op
+  }
+
+  disconnect() {
+    // no-op
+  }
+}
+
+(globalThis as unknown as { ResizeObserver: typeof ResizeObserver }).ResizeObserver = ResizeObserver;
